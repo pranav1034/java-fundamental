6d21cb4ea77fa93a16f05d88a4cf9e751f1254d4
pranav1034 <pranavaggarwal04@gmail.com>
2025-02-27 11:48:34 +0530

    [Pranav] add Rectangle-Perimeter

---
+++ RectanglePerimeter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the length of the rectangle: ");
+        int length = sc.nextInt();
+        System.out.print("Enter the width of the rectangle: ");
+        int width = sc.nextInt();
+        
+        int perimeter = 2 * (length + width);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+}
